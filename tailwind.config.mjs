4b87bd5f2929c148f257ffdeb8d3e9bc73569458
CaoCharles <choun811365@hotmail.com>
2025-12-01 13:48:57 +0800

    style: optimize typography and layout for chinese reading

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,10 +9,32 @@ export default {
                 'news-accent': '#d93025', // A standard news red
             },
             fontFamily: {
-                serif: ['Merriweather', 'serif'],
-                sans: ['Inter', 'sans-serif'],
-            }
+                serif: ['"Playfair Display"', 'Georgia', 'serif'],
+                sans: ['"Microsoft JhengHei"', '"微軟正黑體"', '"Inter"', 'system-ui', 'sans-serif'],
+            },
+            typography: {
+                DEFAULT: {
+                    css: {
+                        maxWidth: '100%',
+                        lineHeight: '1.8',
+                        p: {
+                            marginBottom: '1.25em',
+                        },
+                        'h1, h2, h3, h4': {
+                            fontWeight: '700',
+                            marginTop: '1.5em',
+                            marginBottom: '0.75em',
+                        },
+                        li: {
+                            marginTop: '0.5em',
+                            marginBottom: '0.5em',
+                        },
+                    },
+                },
+            },
         },
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
 }
